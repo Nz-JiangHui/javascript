219b4d6e56b469b793db890c0f2e7d764f9fe104
jianghui <nz.jianghui@gmail.com>
2018-01-12 20:19:22 +0800

    2018.1.12

--- 原型链和继承/index.js
+++ 原型链和继承/index.js
@@ -5,5 +5,14 @@ var obj = {
     value: 2,
     getValue: function () {
         console.log(this.value)
+        return this.value;
     }
 }
+
+obj.getValue();// 2, 此时getValue()的this指向obj
+
+var p = Object.create(obj);
+//p是一个对象，p.__proto__是o
+
+p.value = 4;//创建p的自身属性value,并赋值4
+p.getValue();// 4, 此时getValue()的this指向p;
